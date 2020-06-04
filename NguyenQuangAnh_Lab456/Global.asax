d2911fee32b80522858c3c49308ec840828949dd
Administrator <Administrator@PC262>
2020-06-04 14:07:03 +0700

    Add project files.

---
+++ NguyenQuangAnh_Lab456/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NguyenQuangAnh_Lab456.MvcApplication" Language="C#" %>
---
+++ NguyenQuangAnh_Lab456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NguyenQuangAnh_Lab456.Startup))]
+namespace NguyenQuangAnh_Lab456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
