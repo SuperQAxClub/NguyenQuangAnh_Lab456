8bc31edc1d03bbafa17270b2267735f6efc0f636
SuperQAxClub <nguyenquanganh38@gmail.com>
2020-06-11 14:27:34 +0700

    Add Course works

--- NguyenQuangAnh_Lab456/ViewModels/CourseViewModel.cs
+++ NguyenQuangAnh_Lab456/ViewModels/CourseViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NguyenQuangAnh_Lab456.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +9,18 @@ namespace NguyenQuangAnh_Lab456.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
         public string Date { get; set; }
+        [Required]
         public string Time { get; set; }
+        [Required]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
     }
 }
